a4f86655868290b9f9e143bd50b3256a98b7f763
Debi Prasad Nayak <debi18348@gmail.com>
2025-02-09 17:46:28 +0530

    Add files via upload

---
+++ Remove Elements/script.js
@@ -0,0 +1,1 @@
+console.log("remove elements using js")
---
+++ Selecting elements2/script.js
@@ -0,0 +1,22 @@
+console.log("Som")
+let boxes = document.getElementsByClassName("box")
+console.log(boxes)
+boxes[2].style.backgroundColor = "blue"
+// this will turn 3rd box to blue
+
+document.getElementById("redbox").style.backgroundColor = "red"
+// this will turn the id "redbox" to color red
+// the boxes whose id is named redbox it will turn them to red
+
+document.querySelector(".box").style.backgroundColor="green"
+// this will turn 1st box to green
+
+document.querySelectorAll(".box").forEach(e=>{
+    e.style.backgroundColor="red"
+})
+// if we want to turn all boxes to same color use this
+
+
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
