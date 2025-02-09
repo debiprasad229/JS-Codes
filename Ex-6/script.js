2c35bbf052b111fbba7e525082bfb34db1cb6c4b
Debi Prasad Nayak <debi18348@gmail.com>
2025-02-09 17:40:37 +0530

    Add files via upload

--- Ex-6/script.js
+++ Ex-6/script.js
@@ -1,1 +1,40 @@
-console.log("hello guys")
+console.log("......")
+
+const addItem = async (item) => {
+    await randomDelay();
+    let div = document.createElement("div");
+    div.innerHTML = item
+    document.body.append(div)
+}
+const randomDelay = () => {
+    return new Promise((resolve, reject) => {
+        timeout = 1 + 6 * Math.random()
+        setTimeout(() => {
+            resolve()
+        }, timeout * 500);
+    }
+    )
+}
+async function main() {
+    let t = setInterval(() => {
+        let last = document.body.lastElementChild;
+        if (last.innerHTML.endsWith("........")) {
+            last.innerHTML = last.innerHTML.slice(0, last.innerHTML.length - 3)
+        }
+        else {
+            last.innerHTML = last.innerHTML + "."
+        }
+
+    }, 50)
+    let text = ["Initialized Hacking now reading your data",
+        " Reading Your Files",
+        "Password Files Detected",
+        "Sending all passwords and personal files to server",
+        "Cleaning up",
+        "Now you've lost your all data"]
+    for (const item of text) {
+        await addItem(item)
+    }
+
+}
+main()
